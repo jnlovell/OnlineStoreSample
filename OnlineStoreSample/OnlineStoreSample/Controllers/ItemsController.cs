c235af8ff8c805fab7f026ec0f48f5989f98c063
Joshua Lovell <Joshua Lovell>
2017-04-25 00:15:53 -0400

    Moved all Item functionality to the ItemsController to take advantage of reusibility and eleminate the use of unessisary controllers. Made data insertion Actions use [HTTPPOST]

--- OnlineStoreSample/OnlineStoreSample/Controllers/ItemsController.cs
+++ OnlineStoreSample/OnlineStoreSample/Controllers/ItemsController.cs
@@ -1,6 +1,5 @@
 ﻿using OnlineStoreSample.Models;
 using OnlineStoreSample.Models.SpecificItemModels;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -18,14 +17,15 @@ namespace OnlineStoreSample.Controllers
         // GET: Items
         public ActionResult Index()
         {
-            var itemList = GetItems();
+            var itemList = _context.Items.Where(i => i.Id > 0);
 
             return View(itemList);
         }
 
         public ActionResult Detail(int Id)
         {
-            var item = GetItems().SingleOrDefault(i => i.Id == Id);
+            var item = _context.Items.SingleOrDefault(i => i.Id == Id);
+
 
             if (item == null)
                 return HttpNotFound();
@@ -33,55 +33,207 @@ namespace OnlineStoreSample.Controllers
             return View(item);
         }
 
-        public ActionResult New()
+        // GET: Items/New/Id
+        //Called to decide which Form to return to the client.
+        //We needed a custome route because if we used Id it would over write each Model ID
+        [Route("Items/New/{typeId}")]
+        public ActionResult New(string typeId)
         {
-            Desktop desktop = new Desktop();
-            List<string> itemTypes = new List<string>
-            {
-                "Camera", "Carrying Bag", "Desktop", "Game Console", "Laptop", "Major Appliance", "Mouse And Keyboard", "Movie", "Software", "Video Game"
-            };
-            return View("ItemForm", itemTypes);
-        }
+            if (typeId == "Default")
+                return View("ItemForm");
 
-        [HttpPost]
-        public ActionResult NewSelected(string itemType)
-        {
-            if (itemType == "Desktop")
+            if (typeId == "Desktop")
             {
                 Desktop desktop = new Desktop();
                 return View("DesktopForm", desktop);
             }
 
+            if (typeId == "Camera")
+            {
+                Camera camera = new Camera();
+                return View("CameraForm", camera);
+            }
 
-            return View();
+            if (typeId == "CarryingBag")
+            {
+                CarryingBag carryingBag = new CarryingBag();
+                return View("CarryingBagForm", carryingBag);
+            }
+
+            if (typeId == "GameConsole")
+            {
+                GameConsole gameConsole = new GameConsole();
+                return View("GameConsoleForm", gameConsole);
+            }
+
+            if (typeId == "Laptop")
+            {
+                Laptop laptop = new Laptop();
+                return View("LaptopForm", laptop);
+            }
+
+            if (typeId == "MajorAppliance")
+            {
+                MajorAppliance majorAppliance = new MajorAppliance();
+                return View("MajorApplianceForm", majorAppliance);
+            }
+
+            if (typeId == "MouseAndKeyBoard")
+            {
+                MouseAndKeyBoard mouseAndKeyBoard = new MouseAndKeyBoard();
+                return View("MouseAndKeyBoardForm", mouseAndKeyBoard);
+            }
+
+            if (typeId == "Movie")
+            {
+                Movie movie = new Movie();
+                return View("MovieForm", movie);
+            }
+
+            if (typeId == "Software")
+            {
+                Software software = new Software();
+                return View("SoftwareForm", software);
+            }
+
+            if (typeId == "VideoGame")
+            {
+                VideoGame videoGame = new VideoGame();
+                return View("VideoGameForm", videoGame);
+            }
+
+
+            return View("ItemForm");
         }
 
-        public List<Item> GetItems()
+        //Returns the DesktopForm
+        [HttpPost]
+        public ActionResult NewDesktop(Desktop desktop)
         {
-            var ItemList = new List<Item>();
+            if (!ModelState.IsValid)
+                return View("DesktopForm", desktop);
 
-            var item1 = new Item
-            {
-                Id = 1,
-                Name = "iPhone",
-                Model = "DKFL453LF",
-                Price = 150.00,
-                SKU = 4575346
-            };
+            _context.Items.Add(desktop);
+            _context.SaveChanges();
 
-            var item2 = new Item
-            {
-                Id = 2,
-                Name = "iPad",
-                Model = "DTRHG45345",
-                Price = 400.00,
-                SKU = 4567535
-            };
+            return View("ItemForm");
+        }
 
-            ItemList.Add(item1);
-            ItemList.Add(item2);
+        //Returns the CameraForm
+        [HttpPost]
+        public ActionResult NewCamera(Camera camera)
+        {
+            if (!ModelState.IsValid)
+                return View("CameraForm", camera);
 
-            return ItemList;
+            _context.Items.Add(camera);
+            _context.SaveChanges();
+
+            return View("ItemForm");
+        }
+
+        //Returns the CarryingBagForm
+        [HttpPost]
+        public ActionResult NewCarryingBag(CarryingBag carryingBag)
+        {
+            if (!ModelState.IsValid)
+                return View("CarryingBagForm", carryingBag);
+
+            _context.Items.Add(carryingBag);
+            _context.SaveChanges();
+
+            return View("ItemForm");
+        }
+
+        //Returns the GameConsoleForm
+        [HttpPost]
+        public ActionResult NewGameConsole(GameConsole gameConsole)
+        {
+            if (!ModelState.IsValid)
+                return View("GameConsoleForm", gameConsole);
+
+            _context.Items.Add(gameConsole);
+            _context.SaveChanges();
+
+            return View("ItemForm");
+        }
+
+        //Returns the LaptopForm
+        [HttpPost]
+        public ActionResult NewLaptop(Laptop laptop)
+        {
+            if (!ModelState.IsValid)
+                return View("LaptopForm", laptop);
+
+            _context.Items.Add(laptop);
+            _context.SaveChanges();
+
+            return View("ItemForm");
+        }
+
+        //Returns the MajorApplianceForm
+        [HttpPost]
+        public ActionResult NewMajorAppliance(MajorAppliance majorAppliance)
+        {
+            if (!ModelState.IsValid)
+                return View("MajorApplianceForm", majorAppliance);
+
+            _context.Items.Add(majorAppliance);
+            _context.SaveChanges();
+
+            return View("ItemForm");
+        }
+
+        //Returns the MouseAndKeyBoardForm
+        [HttpPost]
+        public ActionResult NewMouseAndKeyBoard(MouseAndKeyBoard mouseAndKeyBoard)
+        {
+            if (!ModelState.IsValid)
+                return View("MouseAndKeyBoardForm", mouseAndKeyBoard);
+
+            _context.Items.Add(mouseAndKeyBoard);
+            _context.SaveChanges();
+
+            return View("ItemForm");
+        }
+
+        //Returns the MovieForm
+        [HttpPost]
+        public ActionResult NewMovie(Movie movie)
+        {
+            if (!ModelState.IsValid)
+                return View("MovieForm", movie);
+
+            _context.Items.Add(movie);
+            _context.SaveChanges();
+
+            return View("ItemForm");
+        }
+
+        //Returns the SoftwareForm
+        [HttpPost]
+        public ActionResult NewSoftware(Software software)
+        {
+            if (!ModelState.IsValid)
+                return View("SoftwareForm", software);
+
+            _context.Items.Add(software);
+            _context.SaveChanges();
+
+            return View("ItemForm");
+        }
+
+        //Returns the VideoGameForm
+        [HttpPost]
+        public ActionResult NewVideoGame(VideoGame videoGame)
+        {
+            if (!ModelState.IsValid)
+                return View("VideoGameForm", videoGame);
+
+            _context.Items.Add(videoGame);
+            _context.SaveChanges();
+
+            return View("ItemForm");
         }
     }
 }
